849b451382859ca20efcd7904fc576d941f5c7f7
Seth J. Freeman <cyanidecylinder@gmail.com>
2018-10-04 18:10:00 -0400

    added comments

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 function palindrome(str) {
-
-  var re = /[\W_]/g; var lowRegStr = str.toLowerCase().replace(re, '');
-  var reverseStr = lowRegStr.split('').reverse().join(''); $("#output").val(reverseStr === lowRegStr);
-
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, ''); // Removes Non-word Characters And Underscores
+  var reverseStr = lowRegStr.split('').reverse().join(''); // Reverses string
+  $("#output").val(reverseStr === lowRegStr); // Checks if normal string, and reversed string are the same
 }
 
-$("#submit").on("click", function() {
-
-  palindrome($("#input").val());
+$("#submit").on("click", function() { // Calls palindrome function when user clicks submit button
+  palindrome($("#input").val()); // Gets the input value
 });
